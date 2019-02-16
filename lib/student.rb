0d5bba2af78a605fcfd35efd479f9c2ff6f1cce1
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-16 18:05:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,8 @@
 class Student
-
+  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  
+  def create_table
+    DB[:conn].execute("CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)")
+  end
 end
