5c1c98861214e4106a4b3d5bd3df753eb3485e90
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-16 18:33:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,12 +2,12 @@ class Student
   attr_accessor :grade, :name
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def id
+    @id
+  end
   def initialize(name, grade)
     @grade = grade
     @name = name
-  end
-  def id
-    @id
   end
   def self.create_table
     DB[:conn].execute("CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)")
